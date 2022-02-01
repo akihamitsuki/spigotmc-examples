2dc09ca01ee771bc2bd5a294e778a4738ae0da2c
akihamitsuki <46617616+akihamitsuki@users.noreply.github.com>
2022-02-01 17:14:21 +0900

    不要な記述を削除

--- painting-shot/src/main/java/com/example/paintingshot/Main.java
+++ painting-shot/src/main/java/com/example/paintingshot/Main.java
@@ -5,21 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-  private static Main instance;
-
   @Override
   public void onEnable() {
-    Main.instance = this;
     Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-  }
-
-  /**
-   * Main(Plugin)インスタンスを取得する
-   *
-   * @return Mainインスタンス
-   */
-  public static Main getInstance() {
-    return instance;
   }
 
 }
